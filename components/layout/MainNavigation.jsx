185d788540eda5e382391918ffd78ee0b48dde6c
jeff0518 <cycivs07@icloud.com>
2023-08-15 16:31:02 +0800

    fix: layout css

--- components/layout/MainNavigation.jsx
+++ components/layout/MainNavigation.jsx
@@ -5,26 +5,23 @@ import style from "./MainNavigation.module.scss";
 function MainNavigation() {
   return (
     <header className={style.header}>
-      <Link href="/">
-        <div className={style.loge}>uBike</div>
-      </Link>
+      <div className={style.loge}>
+        <Link href="/">Next uBike</Link>
+      </div>
       <nav>
         <ul>
-          <Link href="/search">
-            <li>顯示全部</li>
-          </Link>
-
-          <Link href="/search/one">
-            <li>只顯示1代</li>
-          </Link>
-
-          <Link href="/search/two">
-            <li>只顯示2代</li>
-          </Link>
-
-          <Link href="/auth">
-            <li>登出</li>
-          </Link>
+          <li>
+            <Link href="/search">顯示全部</Link>
+          </li>
+          <li>
+            <Link href="/search/one">只顯示1代</Link>
+          </li>
+          <li>
+            <Link href="/search/two">只顯示2代</Link>
+          </li>
+          <li>
+            <Link href="/auth">登出</Link>
+          </li>
         </ul>
       </nav>
     </header>
